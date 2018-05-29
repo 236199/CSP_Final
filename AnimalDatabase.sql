9e517a543c88cfea40862dc50864fb81bd4afb79
236199 <35493798+236199@users.noreply.github.com>
2018-05-29 08:08:02 -0500

    Add files via upload
    Animal Database

---
+++ AnimalDatabase.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 29, 2018 at 01:07 PM
+-- Server version: 5.5.57-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `AnimalDatabase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `DogTable`
+--
+
+CREATE TABLE IF NOT EXISTS `DogTable` (
+  `Dog` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `Breed` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `Origin` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`Dog`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
